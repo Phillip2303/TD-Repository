f2a9fc4f0d379c58d3ebba16834a552e8ebb4666
Phillip <phillip.tien.ms@gmail.com>
2023-10-22 17:29:15 +0200

    initial check-in of tower defense game

---
+++ TowerDefense/src/main/java/de/phillip/TowerDefense.java
@@ -0,0 +1,16 @@
+package de.phillip;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class TowerDefense extends Application {
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		primaryStage.setTitle(STYLESHEET_CASPIAN);
+		primaryStage.setHeight(480);
+		primaryStage.setWidth(620);
+		primaryStage.show();
+	}
+
+}
